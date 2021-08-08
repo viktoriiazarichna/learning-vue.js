cb5dce1b9b2477bf2ad6052a5d81aa9891727eb2
viktoriiazarichna <viktoriiazarichna@gmail.com>
2021-08-08 23:25:56 +0300

    lesson1

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img v-bind:alt="imageAlt" :src="imageSrc">
+
+  <div>
+    <div>{{ title }} </div>
+
+    <ul>
+      <li v-for="(item, index) of items" :key="item" @click="onListItemClick(item)">
+        {{item}} - {{index}}
+      <button @click="itemRemove(item, index)"> remove </button>
+      </li>
+    </ul>
+
+    <h1 v-if="counter === 0"> Hello</h1>
+    <h1 v-else> World </h1>
+
+    <Counter :counter="counter" @handleIncClick="incHandler" @handleDecClick="decHandler"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Counter from "./components/Counter";
+import Img from './assets/logo.png'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Counter
+  },
+  data() {
+    return {
+      counter: 0,
+      title: 'Dynamic Text',
+      imageAlt: 'Vue logo',
+      imageSrc: Img,
+      items: [1, 2, 3, 4]
+    }
+  },
+  methods: {
+    onListItemClick(itemVal) {
+      console.log(itemVal)
+    },
+    incHandler() {
+      this.counter++;
+    },
+
+    decHandler() {
+      this.counter--;
+    },
+
+    itemRemove(item) {
+      this.items = this.items.filter(el => el !== item)
+    }
+
   }
 }
 </script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+     {{ counter }}
+    <br>
+    <button
+        :class="['bold', getClassName()]"
+        @click="inc">inc</button>
+    <button @click="$emit('handleDecClick')">dec</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    counter: {
+      type: Number,
+      required: true
+    }
+  },
+    methods: {
+      inc() {
+        this.$emit('handleIncClick')
+      },
+      getClassName() {
+        return this.counter > 0 ? 'isInced' : 'isDecked'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .hello {
+    background: #2c3e50;
+    color: darkgrey;
+  }
+
+  .bold {
+    font-weight: bold;
+  }
+
+  .isInced{
+    color: green;
+  }
+  .isDecked{
+    color: red;
+  }
+</style>
