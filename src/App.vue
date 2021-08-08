f624e9a2c37207a985f0839c31cd54988dc55297
viktoriiazarichna <viktoriiazarichna@gmail.com>
2021-08-09 01:04:31 +0300

    lesson2

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,95 @@
 <template>
-  <img v-bind:alt="imageAlt" :src="imageSrc">
-
   <div>
-    <div>{{ title }} </div>
+    <h1> Input Value: {{inputValue}} </h1>
 
-    <ul>
-      <li v-for="(item, index) of items" :key="item" @click="onListItemClick(item)">
-        {{item}} - {{index}}
-      <button @click="itemRemove(item, index)"> remove </button>
-      </li>
+<!--    <input type="text" :value="inputValue" @input="handleInputChange"/>-->
+<!--    <input type="text" :value="inputValue" @input="inputValue = $event.target.value"/>-->
+    <input type="text" v-model.trim="inputValue"  @keypress.enter="createNewTodo"/>
+
+    <button @click="createNewTodo">create todo</button>
+<!--    <br>-->
+<!--    <input type="checkbox" v-model="isOn"/>-->
+<!--    <br>-->
+<!--    <button @click="isOn = !isOn">toggle checkbox</button>-->
+
+    <ul v-if="todos.length">
+      <li v-for="todo of todos" :key="todo.id">{{todo.id}}  - {{todo.title}} - {{todo.completed}}    </li>
     </ul>
-
-    <h1 v-if="counter === 0"> Hello</h1>
-    <h1 v-else> World </h1>
-
-    <Counter :counter="counter" @handleIncClick="incHandler" @handleDecClick="decHandler"/>
+    <h2 v-else-if="loading"> LOADING </h2>
+    <h2 v-else> NO DATA </h2>
   </div>
+
+  <div v-for="error of errors" :key="error"> {{error}}  </div>
+
 </template>
 
 <script>
-
-import Counter from "./components/Counter";
-import Img from './assets/logo.png'
-
 export default {
   name: 'App',
-  components: {
-    Counter
-  },
   data() {
     return {
-      counter: 0,
-      title: 'Dynamic Text',
-      imageAlt: 'Vue logo',
-      imageSrc: Img,
-      items: [1, 2, 3, 4]
+      isOn: false,
+      inputValue: '',
+      todos: [],
+      loading: false
+
     }
   },
   methods: {
-    onListItemClick(itemVal) {
-      console.log(itemVal)
-    },
-    incHandler() {
-      this.counter++;
-    },
+    // alertHandler() {
+    //   alert('hello');
+    // },
+    // handleInputChange(e) {
+    //   const { target: {value} } = e;
+    //
+    //   if(value.length >= 5) {
+    //     e.preventDefault()
+    //   }
+    // }
+    createNewTodo() {
 
-    decHandler() {
-      this.counter--;
-    },
+      if(!this.inputValue) return;
 
-    itemRemove(item) {
-      this.items = this.items.filter(el => el !== item)
+      const newTodo = {
+        id: Math.random(),
+        title: this.inputValue,
+        completed: false
+      }
+
+      this.todos.unshift(newTodo);
+      this.inputValue = '';
+
+
+    },
+    async fetchTodos() {
+      try {
+        this.loading = true;
+        const res= await fetch('https://jsonplaceholder.typicode.com/todos');
+        this.todos = await res.json();
+
+      } catch (e) {
+        console.log(e)
+      } finally {
+        this.loading = false
+      }
+
     }
 
+  },
+
+  watch: {
+    inputValue() {
+      console.log(typeof this.inputValue, this.inputValue)
+    }
+  },
+
+  //lifecycle
+
+
+  created() {
+    this.fetchTodos()
   }
+
 }
 </script>
 
